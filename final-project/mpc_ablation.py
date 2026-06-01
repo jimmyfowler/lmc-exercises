6daec6d44de6174d6725bc27e52544e0e0c9f302
Jimmy Fowler <jfowler0@uw.edu>
2026-05-31 18:11:18 -0700

    started mpc_ablation module

---
+++ final-project/mpc_ablation.py
@@ -0,0 +1,13 @@
+
+# save a dict with these after every run
+hyperparameters = [
+    "horizon_length",
+    "timestep_size",
+    "trust region size",
+    "max_scp_iterations",
+    "convergence_tolerance",
+    "running_state_costs", # nested dictionary
+    "run"
+]
+
+performance_metric = [""]
